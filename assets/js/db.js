19ff9083ef1362b0264314c96ad1a3f7927bb618
Ricardo Christovão da Silva <48654371+ricardochristovao@users.noreply.github.com>
2019-06-19 19:36:41 -0300

    Site
    Primeiro upload

---
+++ assets/js/db.js
@@ -0,0 +1,127 @@
+var dbricardocdasilva = {
+    "data": [
+        {
+            "id": 0,
+            "titulo": "Pintinhos ao ataque",
+			"categoria": "Esporte",
+            "data": "2019-06-15",
+            "subtitulo": "Parece cena de filme, mas os pintinhos estão prestes a dominar o mundo",
+            "autor": "Ricardo",
+            "imagem": "http://skeavee.com/imagens/13.jpg",
+			"texto": "Bacon ipsum dolor amet in tongue proident ea biltong buffalo, sint porchetta leberkas qui. Minim enim strip steak cillum tenderloin qui. Pancetta shoulder ea tri-tip. Reprehenderit ground round picanha frankfurter ex ut spare ribs, sed short ribs flank pancetta boudin.Eu brisket incididunt dolore lorem non venison. Bresaola elit kevin pariatur, ball tip sausage voluptate cow bacon proident dolor exercitation jerky sed. Boudin ex consequat aliqua jerky cow alcatra. Aliquip shoulder chuck fatback shank anim cupidatat. Pariatur tempor leberkas pork loin.Irure reprehenderit chuck swine labore filet mignon shankle sirloin ipsum pariatur. Ham turkey pork chop kevin. Cupidatat in ball tip, in ut consequat tri-tip capicola et jerky nulla hamburger meatloaf lorem. Meatloaf ad ut kielbasa rump pork belly. Jowl fatback ipsum, aliquip capicola et picanha ullamco short ribs drumstick kielbasa.",
+            "comentario":[],
+            "like":"0"
+        },
+        {
+            "id": 1,
+            "titulo": "Pintinhos ao ataque",
+			"categoria": "Politica",
+            "data": "2019-06-14",
+            "subtitulo": "Parece cena de filme, mas os pintinhos estão prestes a dominar o mundo",
+            "autor": "Ricardo",
+            "imagem": "http://skeavee.com/imagens/13.jpg",
+			"texto": "Bacon ipsum dolor amet in tongue proident ea biltong buffalo, sint porchetta leberkas qui. Minim enim strip steak cillum tenderloin qui. Pancetta shoulder ea tri-tip. Reprehenderit ground round picanha frankfurter ex ut spare ribs, sed short ribs flank pancetta boudin.Eu brisket incididunt dolore lorem non venison. Bresaola elit kevin pariatur, ball tip sausage voluptate cow bacon proident dolor exercitation jerky sed. Boudin ex consequat aliqua jerky cow alcatra. Aliquip shoulder chuck fatback shank anim cupidatat. Pariatur tempor leberkas pork loin.Irure reprehenderit chuck swine labore filet mignon shankle sirloin ipsum pariatur. Ham turkey pork chop kevin. Cupidatat in ball tip, in ut consequat tri-tip capicola et jerky nulla hamburger meatloaf lorem. Meatloaf ad ut kielbasa rump pork belly. Jowl fatback ipsum, aliquip capicola et picanha ullamco short ribs drumstick kielbasa.",
+            "comentario":[],
+            "like":"0"
+        },
+        {
+            "id": 2,
+            "titulo": "Pintinhos ao ataque",
+			"categoria": "Saude",
+            "data": "2019-06-11",
+            "subtitulo": "Parece cena de filme, mas os pintinhos estão prestes a dominar o mundo",
+            "autor": "Ricardo",
+            "imagem": "http://skeavee.com/imagens/7.jpg",
+			"texto": "Bacon ipsum dolor amet in tongue proident ea biltong buffalo, sint porchetta leberkas qui. Minim enim strip steak cillum tenderloin qui. Pancetta shoulder ea tri-tip. Reprehenderit ground round picanha frankfurter ex ut spare ribs, sed short ribs flank pancetta boudin.Eu brisket incididunt dolore lorem non venison. Bresaola elit kevin pariatur, ball tip sausage voluptate cow bacon proident dolor exercitation jerky sed. Boudin ex consequat aliqua jerky cow alcatra. Aliquip shoulder chuck fatback shank anim cupidatat. Pariatur tempor leberkas pork loin.Irure reprehenderit chuck swine labore filet mignon shankle sirloin ipsum pariatur. Ham turkey pork chop kevin. Cupidatat in ball tip, in ut consequat tri-tip capicola et jerky nulla hamburger meatloaf lorem. Meatloaf ad ut kielbasa rump pork belly. Jowl fatback ipsum, aliquip capicola et picanha ullamco short ribs drumstick kielbasa.",
+            "comentario":[],
+            "like":"0"
+        },{
+            "id": 3,
+            "titulo": "Pintinhos ao ataque",
+			"categoria": "Saude",
+            "data": "2019-06-11",
+            "subtitulo": "Parece cena de filme, mas os pintinhos estão prestes a dominar o mundo",
+            "autor": "Ricardo",
+            "imagem": "http://skeavee.com/imagens/6.jpg",
+			"texto": "Bacon ipsum dolor amet in tongue proident ea biltong buffalo, sint porchetta leberkas qui. Minim enim strip steak cillum tenderloin qui. Pancetta shoulder ea tri-tip. Reprehenderit ground round picanha frankfurter ex ut spare ribs, sed short ribs flank pancetta boudin.Eu brisket incididunt dolore lorem non venison. Bresaola elit kevin pariatur, ball tip sausage voluptate cow bacon proident dolor exercitation jerky sed. Boudin ex consequat aliqua jerky cow alcatra. Aliquip shoulder chuck fatback shank anim cupidatat. Pariatur tempor leberkas pork loin.Irure reprehenderit chuck swine labore filet mignon shankle sirloin ipsum pariatur. Ham turkey pork chop kevin. Cupidatat in ball tip, in ut consequat tri-tip capicola et jerky nulla hamburger meatloaf lorem. Meatloaf ad ut kielbasa rump pork belly. Jowl fatback ipsum, aliquip capicola et picanha ullamco short ribs drumstick kielbasa.",
+            "comentario":[],
+            "like":"0"
+        },{
+            "id": 4,
+            "titulo": "Pintinhos ao ataque",
+			"categoria": "Saude",
+            "data": "2019-06-11",
+            "subtitulo": "Parece cena de filme, mas os pintinhos estão prestes a dominar o mundo",
+            "autor": "Ricardo",
+            "imagem": "http://skeavee.com/imagens/5.jpg",
+			"texto": "Bacon ipsum dolor amet in tongue proident ea biltong buffalo, sint porchetta leberkas qui. Minim enim strip steak cillum tenderloin qui. Pancetta shoulder ea tri-tip. Reprehenderit ground round picanha frankfurter ex ut spare ribs, sed short ribs flank pancetta boudin.Eu brisket incididunt dolore lorem non venison. Bresaola elit kevin pariatur, ball tip sausage voluptate cow bacon proident dolor exercitation jerky sed. Boudin ex consequat aliqua jerky cow alcatra. Aliquip shoulder chuck fatback shank anim cupidatat. Pariatur tempor leberkas pork loin.Irure reprehenderit chuck swine labore filet mignon shankle sirloin ipsum pariatur. Ham turkey pork chop kevin. Cupidatat in ball tip, in ut consequat tri-tip capicola et jerky nulla hamburger meatloaf lorem. Meatloaf ad ut kielbasa rump pork belly. Jowl fatback ipsum, aliquip capicola et picanha ullamco short ribs drumstick kielbasa.",
+            "comentario":[],
+            "like":"0"
+        }
+    ]
+}
+
+// Caso exista no Local Storage, recupera os dados salvos
+var db = JSON.parse(localStorage.getItem('dbRicardoChristovao'));
+if (!db) {
+    localStorage.setItem('dbRicardoChristovao',JSON.stringify(dbricardocdasilva));
+};
+
+// Exibe mensagem em um elemento de ID msg
+function displayMessage(msg) {
+    $('#msg').html('<div class="alert alert-warning">' + msg + '</div>');
+}
+
+function insertContato(contato) {
+    // Calcula novo Id a partir do último código existente no array
+    let novoId = db.data[db.data.length - 1].id + 1;
+    let novoContato = {
+        "id": novoId,
+        "titulo": contato.titulo,
+		"categoria": contato.categoria,
+        "data": contato.data,
+        "subtitulo": contato.subtitulo,
+        "autor": contato.autor,
+		"imagem": contato.imagem, 
+        "texto": contato.texto
+    };
+
+    // Insere o novo objeto no array
+    db.data.push(novoContato);
+    displayMessage("Noticia inserida com sucesso");
+
+    // Atualiza os dados no Local Storage
+    localStorage.setItem('dbRicardoChristovao', JSON.stringify(db));
+}
+
+function updateContato(id, contato) {
+    // Localiza o indice do objeto a ser alterado no array a partir do seu ID
+    let index = db.data.map(obj => obj.id).indexOf(id);
+
+    // Altera os dados do objeto no array
+		db.data[index].titulo = contato.titulo,
+		db.data[index].categoria = contato.categoria,
+        db.data[index].data = contato.data,
+        db.data[index].subtitulo = contato.subtitulo,
+        db.data[index].autor = contato.autor,
+		db.data[index].imagem = contato.imagem,
+		db.data[index].texto = contato.texto
+
+    displayMessage("Noticia alterada com sucesso");
+
+    // Atualiza os dados no Local Storage
+    localStorage.setItem('dbRicardoChristovao', JSON.stringify(db));
+}
+
+function deleteContato(id) {    
+    // Filtra o array removendo o elemento com o id passado
+    db.data = db.data.filter(function (element) { return element.id != id });
+
+    displayMessage("Noticia removida com sucesso");
+
+    // Atualiza os dados no Local Storage
+    localStorage.setItem('db', JSON.stringify(db));
+}
+
+var element = document.getElementById("result");
+	
